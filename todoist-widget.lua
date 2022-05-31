b02d21c813a98cf97eeaf98ea98e9db8d20f7f51
Evgeny <zobnin@gmail.com>
2022-05-31 13:31:54 +0300

    Big refacroting & UI cleanup & fix small bugs

--- todoist-widget.lua
+++ todoist-widget.lua
@@ -3,7 +3,7 @@
 -- description = "Integration with Todoist"
 -- data_source = "https://todoist.com/app/"
 -- author = "Timo Peters & Andrey Gavrilov"
--- version = "2.0
+-- version = "2.1
 -- arguments_default = "0 0"
 
 -- modules
@@ -11,8 +11,14 @@ local json = require "json"
 local md_colors = require "md_colors"
 
 -- constants
-local base_uri = "https://api.todoist.com/rest/v1/"
-local colors = {md_colors.red_500, md_colors.orange_500, md_colors.blue_500}
+local decay_time = 6 * 60 * 60
+local white = ui:get_colors().primary_text
+local grey = ui:get_colors().secondary_text
+local colors = {
+    md_colors.red_500,
+    md_colors.orange_500,
+    md_colors.blue_500,
+}
 
 -- vars
 local projects = {}
@@ -24,26 +30,29 @@ local task = 0
 
 function on_alarm()
 	local args = settings:get()
-	if not settings:get() then
+
+    if not settings:get() then
 		settings:set({0, 0})
 	end
+
 	local token = settings:get()[1]
-	if tonumber(token) == 0 then
+    if tonumber(token) == 0 then
         ui:show_text("Tap to enter your API token!")
         return
 	end
-	http:set_headers({ "Authorization: Bearer " .. token })
-	http:get(base_uri .. "projects", "projects")
+
+    api_set_token(token)
+    api_get_projects()
 end
 
 function on_network_result_projects(res)
 	files:write("todoist_projects", res)
-	http:get(base_uri .. "sections", "sections")
+    api_get_sections()
 end
 
 function on_network_result_sections(res)
 	files:write("todoist_sections", res)
-	http:get(base_uri .. "tasks", "tasks")
+    api_get_tasks()
 end
 
 function on_network_result_tasks(res, err)
@@ -62,10 +71,12 @@ function on_resume()
 	if not files:read("todoist_tasks") then
 		files:write("todoist_tasks", json.encode({}))
 	end
-	projects = json.decode(files:read("todoist_projects"))
+
+    projects = json.decode(files:read("todoist_projects"))
 	sections = json.decode(files:read("todoist_sections"))
 	tasks = json.decode(files:read("todoist_tasks"))
-	redraw()
+
+    redraw()
 end
 
 function on_click(idx)
@@ -81,97 +92,28 @@ function on_click(idx)
 	end
 end
 
-function on_settings()
-	dialog_id = "settings"
-	ui:show_edit_dialog("Enter your API token", "", settings:get()[1])
-end
-
-function on_dialog_action(res)
-	if res == -1 and dialog_id ~= "task" then
-		return
-	end
-	if dialog_id == "settings" then
-		local args = settings:get()
-		args[1] = res
-		settings:set(args)
-		on_alarm()
-	elseif dialog_id == "projects" then
-		local args = settings:get()
-	    if res > 1 then
-	        args[2] = projects[res-1].id
-	    else
-	        args[2] = 0
-	    end
-		settings:set(args)
-		on_resume()
-	elseif dialog_id == "task" then
-	    if res == -1 then
-	        http:delete(base_uri .. "tasks/" .. task, "delete")
-	    else
-	        local priority = 1
-            if res.color < 6 then
-                priority = 5 - res.color
-            end
-            local datetime = os.date("%Y-%m-%dT%H:%M:00Z", res.due_date - system:get_tz_offset())
-            local body = {
-                content = res.text,
-                priority = priority,
-                due_datetime = datetime
-            }
-            http:post(base_uri .. "tasks/" .. task, json.encode(body), "application/json", "task")
-	    end
-	elseif dialog_id == "create" then
-        local priority = 1
-        if res.color < 6 then
-            priority = 5 - res.color
-        end
-        local datetime = os.date("%Y-%m-%dT%H:%M:00Z", os.time() - system:get_tz_offset())
-        local body = {
-            content = res.text,
-            priority = priority,
-            due_datetime = datetime
-        }
-        local project = tonumber(settings:get()[2])
-        if project > 0 then
-            body.project_id = project
-        end
-        http:post(base_uri .. "tasks/", json.encode(body), "application/json", "create")
-    end
-end
-
-function redraw()
-    local lines = {}
-	lines_id = {}
-    local project = tonumber(settings:get()[2])
-	local line = "<b>"
-    if project == 0 then
-		line = line .. "All projects"
-    else
-		line = line .. get_project_name(project)
-    end
-	line = line .. "</b>"
-	if os.time() - files:read("todoist_time") > 24 * 60 * 60 then
-	    line = line .. " (Data is outdated)"
-	end
-	table.insert(lines, line)
-	table.insert(lines_id, project)
-	lines = insert_tasks(lines, project, 0)
-    for i,v in ipairs(sections) do
-		lines = insert_tasks(lines, project, v.id)
-    end
-    table.insert(lines, "<font color=\"" .. ui:get_colors().secondary_text .. "\">Add task</font>")
-    table.insert(lines_id, 0)
-    ui:show_lines(lines)
-end
-
 function select_project()
     local tab = {}
     table.insert(tab, "All projects")
     for i,v in ipairs(projects) do
         table.insert(tab, v.name)
     end
+
     dialog_id = "projects"
     ui:show_radio_dialog("Select project", tab, get_project_idx() + 1)
+end
+
+function on_dialog_projects(res)
+    local args = settings:get()
+
+    if res > 1 then
+        args[2] = projects[res-1].id
+    else
+        args[2] = 0
+    end
+
+    settings:set(args)
+    on_resume()
 end
 
 function get_project_idx()
@@ -182,6 +124,49 @@ function get_project_idx()
         end
     end
     return 0
+end
+
+function on_settings()
+	dialog_id = "settings"
+	ui:show_edit_dialog("Enter your API token", "", settings:get()[1])
+end
+
+function on_dialog_settings(res)
+    local args = settings:get()
+    args[1] = res
+    settings:set(args)
+    on_alarm()
+end
+
+function redraw()
+    local lines = {}
+	local line = ""
+	lines_id = {}
+    local project = tonumber(settings:get()[2])
+
+    if project == 0 then
+		line = line.."All projects"
+    else
+		line = line..get_project_name(project)
+    end
+
+    if os.time() - files:read("todoist_time") > decay_time then
+	    line = "<b>"..line.."</b>".." (outdated)"
+	end
+
+    table.insert(lines, line)
+	table.insert(lines_id, project)
+
+    lines = insert_tasks(lines, project, 0)
+
+    for i,v in ipairs(sections) do
+		lines = insert_tasks(lines, project, v.id)
+    end
+
+    table.insert(lines, colored("Add task", grey))
+    table.insert(lines_id, 0)
+
+    ui:show_lines(lines)
 end
 
 function get_project_name()
@@ -204,6 +189,7 @@ end
 
 function insert_tasks(tab, pr, sec)
 	local is_sec = true
+
 	for i,v in ipairs(tasks) do
 		if ((pr == v.project_id) or (pr == 0)) and (sec == v.section_id) and (not v.parent) and not v.completed then
 			if is_sec and sec ~= 0 then
@@ -211,20 +197,12 @@ function insert_tasks(tab, pr, sec)
 				table.insert(lines_id, sec)
 				is_sec = false
 			end
-			local color = ui:get_colors().primary_text
-			if v.priority > 1 then
-			    color = colors[5 - v.priority]
-			end
-			local line = "%%mkd%%<font color=\"" .. color .. "\">&#8627; " .. v.content .. "</font>"
-			local due_date = get_time(v.due)
-			if due_date ~= nil then
-			    line = line .. "<font color=\"" .. ui:get_colors().secondary_text .. "\"> - " .. os.date("%d %b, %H:%M", due_date) .. "</font>"
-			    if due_date < os.time() then
-			        line = line .. " (*)"
-			    end
-			end
-			table.insert(tab, line)
+
+			local line = task_text(v)..task_date(v)
+
+            table.insert(tab, line)
 			table.insert(lines_id, v.id)
+
 			tab = insert_subtasks(tab, v.id, 1)
 		end
 	end
@@ -232,26 +210,19 @@ function insert_tasks(tab, pr, sec)
 end
 
 function insert_subtasks(tab, id, lev)
+    local curr_time = os.time()
+
 	for i,v in ipairs(tasks) do
 		if (v.parent_id == id) and not v.completed then
-			local color = ui:get_colors().primary_text
-			if v.priority > 1 then
-			    color = colors[5 - v.priority]
+			local line = task_text(v)..task_date()
+
+            for i = 1, lev do
+				line = "&nbsp;&nbsp;&nbsp;"..line
 			end
-			local line = "<font color=\"" .. color .. "\">&#8627; " .. v.content .. "</font>"
-			for i = 1, lev do
-				line = "&nbsp;&nbsp;&nbsp;" .. line
-			end
-			line = "%%mkd%%" .. line
-			local due_date = get_time(v.due)
-			if due_date ~= nil then
-			    line = line .. "<font color=\"" .. ui:get_colors().secondary_text .. "\"> - " .. os.date("%d %b, %H:%M", due_date) .. "</font>"
-			    if due_date < os.time() then
-			        line = line .. " (*)"
-			    end
-			end
+
 			table.insert(tab, line)
 			table.insert(lines_id, v.id)
+
 			tab = insert_subtasks(tab, v.id, lev + 1)
 		end
 	end
@@ -263,20 +234,32 @@ function open_task(id)
 		if v.id == id then
 			dialog_id = "task"
 			task = v.id
-			local color = 6
+
+            local color = 6
 			if v.priority > 1 then
 			    color = 5 - v.priority
 			end
-            ui:show_rich_editor({
-                text = v.content .. "\n" .. v.description,
+
+            ui:show_rich_editor{
+                text = v.content.."\n"..v.description,
                 due_date = get_time(v.due),
                 colors = colors,
                 color = color,
                 new = false
-        })
-			return
+            }
+
+            return
 		end
 	end
+end
+
+function on_dialog_task(res)
+    if res == -1 then
+        api_delete_task(task)
+    else
+        local body = create_task_json(res)
+        api_edit_task(task, body)
+    end
 end
 
 function on_long_click(idx)
@@ -288,7 +271,10 @@ function open_context_menu(id)
 		if v.id == id then
 			dialog_id = "task"
 			task = v.id
-			ui:show_context_menu({ { "check", "Done" }, { "trash", "Delete" } })
+			ui:show_context_menu{
+                { "check", "Done" },
+                { "trash", "Delete" }
+            }
 			return
 		end
 	end
@@ -296,7 +282,9 @@ function open_context_menu(id)
 		if v.id == id then
 			dialog_id = "section"
 			task = v.id
-			ui:show_context_menu({ { "trash", "Delete" } })
+			ui:show_context_menu{
+                { "trash", "Delete" }
+            }
 			return
 		end
 	end
@@ -304,7 +292,9 @@ function open_context_menu(id)
 		if (v.id == id) and (get_project_name(id) ~= "Inbox") then
 			dialog_id = "project"
 			task = v.id
-			ui:show_context_menu({ { "trash", "Delete" } })
+			ui:show_context_menu{
+                { "trash", "Delete" }
+            }
 			return
 		end
 	end
@@ -313,15 +303,31 @@ end
 function on_context_menu_click(idx)
 	if dialog_id == "task" then
 		if idx == 1 then
-			http:post(base_uri .. "tasks/" .. task .. "/close", "", "application/json", "close")
+            api_close_task()
 		elseif idx == 2 then
-			http:delete(base_uri .. "tasks/" .. task, "delete")
+            api_delete_task()
 		end
 	elseif dialog_id == "section" then
-		http:delete(base_uri .. "sections/" .. task, "delete_sec")
+        api_delete_section(task)
 	elseif dialog_id == "project" then
-		http:delete(base_uri .. "projects/" .. task, "delete_pr")
+        api_delete_project(task)
 	end
+end
+
+function on_dialog_action(res)
+	if res == -1 and dialog_id ~= "task" then
+		return
+	end
+
+    if dialog_id == "settings" then
+        on_dialog_settings(res)
+	elseif dialog_id == "projects" then
+        on_dialog_projects(res)
+	elseif dialog_id == "task" then
+        on_dialog_task(res)
+	elseif dialog_id == "create" then
+        on_dialog_create(res)
+    end
 end
 
 function on_network_result_close(res, err)
@@ -330,6 +336,7 @@ function on_network_result_close(res, err)
         on_alarm()
         return
     end
+
     ui:show_toast("There was an error closing the task!")
 end
 
@@ -339,6 +346,7 @@ function on_network_result_delete(res, err)
         on_alarm()
         return
     end
+
     ui:show_toast("There was an error deleting the task!")
 end
 
@@ -348,6 +356,7 @@ function on_network_result_delete_sec(res, err)
         on_alarm()
         return
     end
+
     ui:show_toast("There was an error deleting the section!")
 end
 
@@ -363,33 +372,21 @@ function on_network_result_delete_pr(res, err)
     ui:show_toast("There was an error deleting the project!")
 end
 
-function on_network_error_close()
-	show_no_connection()
-end
-
-function on_network_error_delete()
-	show_no_connection()
-end
-
-function on_network_error_delete_sec()
-	show_no_connection()
-end
-
-function on_network_error_delete_pr()
-	show_no_connection()
-end
-
-function show_no_connection()
-	ui:show_toast("No connection!")
-end
-
 function create_task()
     dialog_id = "create"
-    ui:show_rich_editor({
-        due_date = os.time(),
+
+    ui:show_rich_editor{
+        due_date = 0,
         colors = colors,
         color = 6
-        })
+    }
+end
+
+function on_dialog_create(res)
+    local project = tonumber(settings:get()[2])
+    local body = create_task_json(res, project)
+
+    api_create_task(body)
 end
 
 function on_network_result_create(res, err)
@@ -398,14 +395,107 @@ function on_network_result_create(res, err)
         on_alarm()
         return
     end
+
     ui:show_toast("There was an error creating the task!")
+end
+
+function on_network_result_task(res, err)
+    if err == 204 then
+		ui:show_toast("Task updated!")
+        on_alarm()
+        return
+    end
+
+    ui:show_toast("There was an error updating the task!")
+end
+
+function on_network_error_create() show_no_connection() end
+function on_network_error_task() show_no_connection() end
+function on_network_error_close() show_no_connection() end
+function on_network_error_delete() show_no_connection() end
+function on_network_error_delete_sec() show_no_connection() end
+function on_network_error_delete_pr() show_no_connection() end
+
+function show_no_connection()
+    ui:show_toast("No connection!")
+end
+
+-- Utils
+
+function create_task_json(res, project)
+    local priority = 1
+    if res.color < 6 then
+        priority = 5 - res.color
+    end
+
+    local body = {
+        content = res.text,
+        priority = priority,
+        due_datetime = rfc822_date(res.due_date)
+    }
+
+    if project ~= nil and project > 0 then
+        body.project_id = project
+    end
+
+    return json.encode(body)
+end
+
+-- Int date to RFC822 date
+function rfc822_date(date)
+    return os.date("%Y-%m-%dT%H:%M:00Z", date - system:get_tz_offset())
+end
+
+-- HTML-colored string
+function colored(str, color)
+    return "<font color=\""..color.."\">"..str.."</font>"
+end
+
+function task_text(v)
+	local due_date = get_time(v.due)
+    local dot = dot(due_date)
+    local color = color_by_priority(v.priority)
+
+	return "%%mkd%%"..colored(dot.." "..v.content, color)
+end
+
+function task_date(v)
+	local due_date = get_time(v.due)
+
+    if due_date ~= nil then
+	    return colored(" - "..os.date("%d %b, %H:%M", due_date), grey)
+    end
+
+    return ""
+end
+
+-- Dot in the beggining of task
+function dot(due_date)
+    local circle = "⬤ "
+
+    if due_date ~= nil and due_date < os.time() then
+        circle = "❗"
+    end
+
+    return circle
+end
+
+function color_by_priority(priority)
+    local color = white
+
+    if priority > 1 then
+        color = colors[5 - priority]
+    end
+
+    return color
 end
 
 function get_time(due)
 	local due_date = nil
 	local due_time = nil
 	local offset = 0
-	if due ~= nil then
+
+    if due ~= nil then
 		if due.datetime ~= nil then
 		    due_time = due.datetime:split("T")
 			due_time = due_time[2]:split(":")
@@ -415,25 +505,62 @@ function get_time(due)
 		end
 		due_date = due.date:split("-")
 	end
-	if due_date ~= nil then
-		due_date = os.time{year = due_date[1], month = due_date[2], day = due_date[3], hour = due_time[1], min = due_time[2], sec = 0} + offset
+
+    if due_date ~= nil then
+		due_date = os.time{
+            year = due_date[1],
+            month = due_date[2],
+            day = due_date[3],
+            hour = due_time[1],
+            min = due_time[2],
+            sec = 0
+        } + offset
 	end
-	return due_date
+
+    return due_date
 end
 
-function on_network_result_task(res, err)
-    if err == 204 then
-		ui:show_toast("Task updated!")
-        on_alarm()
-        return
-    end
-    ui:show_toast("There was an error updating the task!")
+-- Todoist API
+
+local base_uri = "https://api.todoist.com/rest/v1/"
+
+function api_set_token(token)
+	http:set_headers{ "Authorization: Bearer "..token }
 end
 
-function on_network_error_create()
-	show_no_connection()
+function api_get_projects()
+	http:get(base_uri .. "projects", "projects")
 end
 
-function on_network_error_task()
-	show_no_connection()
+function api_get_sections()
+	http:get(base_uri .. "sections", "sections")
 end
+
+function api_get_tasks()
+	http:get(base_uri .. "tasks", "tasks")
+end
+
+function api_create_task(body)
+    http:post(base_uri .. "tasks/", body, "application/json", "create")
+end
+
+function api_delete_task(task)
+    http:delete(base_uri.."tasks/"..task, "delete")
+end
+
+function api_edit_task(task, body)
+    http:post(base_uri.."tasks/"..task, body, "application/json", "task")
+end
+
+function api_close_task(task)
+	http:post(base_uri .. "tasks/" .. task .. "/close", "", "application/json", "close")
+end
+
+function api_delete_section(task)
+    http:delete(base_uri .. "sections/" .. task, "delete_sec")
+end
+
+function api_delete_project(task)
+    http:delete(base_uri .. "projects/" .. task, "delete_pr")
+end
+
