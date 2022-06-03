2b0db64c899a982ee035772e700859a5ef759822
Evgeny <zobnin@gmail.com>
2022-06-03 20:13:09 +0300

    fix local dates handling

--- todoist-widget.lua
+++ todoist-widget.lua
@@ -639,7 +639,11 @@ end
 function parse_due_date(due)
     if due ~= nil then
         if due.datetime ~= nil then
-            return parse_iso8601_date(due.datetime) + system:get_tz_offset()
+            local offset = 0
+            if due.datetime:find('Z') then
+                offset = system:get_tz_offset()
+            end
+            return parse_iso8601_date(due.datetime) + offset
         end
     end
 
@@ -651,13 +655,13 @@ function to_iso8601_date(date)
 end
 
 function parse_iso8601_date(json_date)
-    local pattern = "(%d+)%-(%d+)%-(%d+)%a(%d+)%:(%d+)%:([%d%.]+)([Z%+%- ])(%d?%d?)%:?(%d?%d?)"
+    local pattern = "(%d+)%-(%d+)%-(%d+)%a(%d+)%:(%d+)%:([%d%.]+)([Z%+%-]?)(%d?%d?)%:?(%d?%d?)"
     local year, month, day, hour, minute,
         seconds, offsetsign, offsethour, offsetmin = json_date:match(pattern)
     local timestamp = os.time{year = year, month = month,
         day = day, hour = hour, min = minute, sec = seconds}
     local offset = 0
-    if offsetsign ~= 'Z' then
+    if offsetsign ~= '' and offsetsign ~= 'Z' then
       offset = tonumber(offsethour) * 60 + tonumber(offsetmin)
       if xoffset == "-" then offset = offset * -1 end
     end
